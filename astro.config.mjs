5b2b9a07caff821192923368ccaf6a779f8e4a24
carimatics <github.carimatics@gmail.com>
2026-02-09 02:38:44 +0900

    Migrate blog posts to localized structure, integrate React support, and enhance "About" page with dynamic content and components.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
+  base: '/demo',
   trailingSlash: "never",
+  integrations: [react()],
+  i18n: {
+    locales: ["ja", "en"],
+    defaultLocale: "ja",
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  },
 });
